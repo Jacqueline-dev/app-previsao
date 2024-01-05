af2d86ddadb983421f8e959cd28821e909f62f0c
Jacqueline-dev <codejacque@gmail.com>
2024-01-05 15:19:09 +0100

    buscando dados da API

---
+++ src/js/index.js
@@ -0,0 +1,50 @@
+const chaveDaApi = ""
+
+const botaoDeBusca = document.querySelector(".btn-busca");
+
+botaoDeBusca.addEventListener("click", async () => {
+    const cidade = document.getElementById("input-busca").value;
+
+    if (!cidade) return;
+
+    const dados = await buscaDadosDaCidade(cidade);
+
+    if (dados) preencherDadosNaTela(dados, cidade);
+
+});
+
+async function buscarDadosDaCidade(cidade) {
+    const apiUrl = ``;
+
+    const resposta = await fetch(apiUrl);
+
+    if (reposta.status !== 200) return;
+
+    const dados = resposta.json();
+
+    return dados;
+}
+
+
+function preencherDadosNaTela(dados, cidade) {
+    const temperatura = dados.current.temp_c;
+    const condicao = dados.current.condition.text;
+    const humidade = dados.current.humidity;
+    const velocidadeDoVento = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon;
+
+
+document.getElementById("cidade").textContent = cidade;
+
+document.getElementById("temperatura").textContent = `${temperatura} ºC`;
+document.getElementById("condicao").textContent = condicao;
+document.getElementById("humidade").textContent = `${humidade}%`;
+
+document.getElementById(
+    "velocidade-do-vento"
+  ).textContent = `${velocidadeDoVento} km/h`;
+
+  document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+}
+
+
